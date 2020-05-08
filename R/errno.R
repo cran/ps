b3064b9b8f7934e34bfb4d2d321a60b7bed9daa9
Gábor Csárdi <csardi.gabor@gmail.com>
2020-05-08 07:50:02 +0000

    version 1.3.3

---
+++ R/errno.R
@@ -0,0 +1,22 @@
+
+#' List of 'errno' error codes
+#'
+#' For the errors that are not used on the current platform, `value` is
+#' `NA_integer_`.
+#'
+#' A data frame with columns: `name`, `value`, `description`.
+#' @export
+#' @examplesIf ps::ps_is_supported()
+#' errno()
+
+errno <- function() {
+  err <- as.list(ps_env$constants$errno)
+  err <- err[order(names(err))]
+  data.frame(
+    stringsAsFactors = FALSE,
+    row.names = NULL,
+    name = names(err),
+    value = vapply(err, "[[", integer(1), 1),
+    description = vapply(err, "[[", character(1), 2)
+  )
+}
